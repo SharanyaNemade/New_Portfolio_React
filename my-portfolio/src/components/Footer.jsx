fa43321f70d50354b297fc84e643d0d79be572f5
Sharanya Nemade <87638228+SharanyaNemade@users.noreply.github.com>
2025-10-15 22:53:54 +0530

    Create Footer.jsx

---
+++ my-portfolio/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../css/Footer.css";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>Made with ❤️ by Sharanya Nemade • © 2025</p>
+            <p>&copy; 2025 Sharanya Nemade. All rights reserved.</p>
+        </footer>
+    );
+}
